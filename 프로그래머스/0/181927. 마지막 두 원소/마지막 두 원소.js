a81bdddecd91101dd88933353f37ce1c6062df75
배정태 <61902400+dkagh012@users.noreply.github.com>
2024-02-25 21:43:10 +0900

    [level 0] Title: 마지막 두 원소, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181927. 마지막 두 원소/마지막 두 원소.js
+++ 프로그래머스/0/181927. 마지막 두 원소/마지막 두 원소.js
@@ -1,5 +1,9 @@
 function solution(num_list) {
     var answer = [];
-    let result = num_list[num_list.length - 1] > num_list[num_list.length - 2] ? num_list.push(num_list[num_list.length - 1] - num_list[num_list.length - 2]) : num_list.push(num_list[num_list.length - 1] * 2);
+    let lastNum = num_list[num_list.length - 1];
+    let secondLastNum = num_list[num_list.length - 2];
+    
+    num_list.push(lastNum > secondLastNum ? lastNum - secondLastNum : lastNum * 2);
+    
     return num_list;
 }
